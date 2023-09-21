4778791a1e761830e103f26b546b4f8fbb415603
grey <starbux630@naver.com>
2023-09-22 02:19:20 +0900

    feat: make server and view html page by gin

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.LoadHTMLGlob("./web/templates/*.html")
+
+	// HTML 템플릿 파일을 렌더링하는 라우터
+	router.GET("/", func(c *gin.Context) {
+		// HTML 렌더링
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	router.GET("/greeting", func(c *gin.Context) {
+		// HTML 렌더링
+		c.String(http.StatusOK, "HELLO WORLD!")
+	})
+
+	router.Run(":8080")
+}
